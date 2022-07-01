22d696cf40595c89bdca92312144838bbaf42650
luciaduque248 <luciaduque248@gmail.com>
2022-06-30 19:03:36 -0500

    all was modified

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 
 import AppRoutes from './routes/AppRoutes';
 
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppRoutes/>
