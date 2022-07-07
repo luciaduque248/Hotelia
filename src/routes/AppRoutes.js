203ae6a7089b5806acc75a5b7fe174f6daec876a
CADALASO <cadalaso1402@gmail.com>
2022-07-07 03:01:40 -0500

    NavBar components for Admin Dashboard and User Dashboard are made, and work is being done on the login interface to User Dashboard and Admin Dashboard.

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
+import Admin_Dashboard from '../admin/Dashboard'
 import Inicio from '../pages/Inicio'
+import User_Dashboard from '../user/Dashboard'
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/Hotelia/' element={<Inicio/>} ></Route>
-
-        </Routes>
+      <Routes>
+        <Route path='/Hotelia/' element={<Inicio />} ></Route>
+        <Route path='/Admin-Dashboard' element={<Admin_Dashboard />}></Route>
+        <Route path='/User-Dashboard' element={<User_Dashboard/>}></Route>
+      </Routes>
     </BrowserRouter>
   )
 }
