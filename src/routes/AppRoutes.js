9027c19265a32582a57c50a810cc5b0c07a328d4
luciaduque248 <luciaduque248@gmail.com>
2022-07-08 00:54:03 -0500

    list of rooms was added and modified

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ListHabs from '../Admin/ListHabs'
 
 import Inicio from '../pages/Inicio'
 
@@ -8,6 +9,9 @@ function AppRoutes() {
     <BrowserRouter>
         <Routes>
             <Route path='/Hotelia/' element={<Inicio/>} ></Route>
+
+
+            <Route path='/Hotelia/list-habitaciones' element={<ListHabs/>} ></Route>
 
         </Routes>
     </BrowserRouter>
